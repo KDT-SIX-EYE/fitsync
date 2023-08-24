60a95430140e3106b7bc44c2366aa9a5af9fe98d
vmkmym <71699054+vmkmym@users.noreply.github.com>
2023-08-24 16:24:23 +0900

    Initial commit

---
+++ app/src/main/java/com/example/fitsync/BookActivity.kt
@@ -0,0 +1,18 @@
+package com.example.fitsync
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.fitsync.ui.theme.FitSyncTheme
+
+class BookActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContent {
+            FitSyncTheme {
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/fitsync/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.fitsync
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.example.fitsync.ui.theme.FitSyncTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FitSyncTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column {
+                        val context = LocalContext.current
+                        Button(onClick = {
+                            val intent = Intent(context, CalenderActivity::class.java)
+                            context.startActivity(intent)
+                        }) {
+                            Text(text = "캘린더")
+                        }
+                        Button(onClick = {
+                            val intent = Intent(context, ScheduleActivity::class.java)
+                            context.startActivity(intent)
+                        }) {
+                            Text(text = "스케줄")
+                        }
+                        Button(onClick = {
+                            val intent = Intent(context, BookActivity::class.java)
+                            context.startActivity(intent)
+                        }) {
+                            Text(text = "예약")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
