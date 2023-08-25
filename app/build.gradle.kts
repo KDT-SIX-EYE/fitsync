df88664fb7fa5155eccb8b8d4824dae8ad3450bd
JD131313 <dbalfh@gmail.com>
2023-08-25 10:46:33 +0900

    update caleder1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -59,6 +60,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+//    implementation("com.google.firebase:firebase-firestore-ktx:24.7.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -66,4 +70,10 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation ("com.google.firebase:firebase-firestore-ktx:24.7.1")
+    implementation(platform("com.google.firebase:firebase-bom:32.2.3"))
+    implementation("com.google.firebase:firebase-firestore-ktx")
+
+    implementation ("androidx.compose.material:material-icons-extended:1.2.0")
 }
--- app/src/main/java/com/example/fitsync/CalenderActivity.kt
+++ app/src/main/java/com/example/fitsync/CalenderActivity.kt
@@ -1,20 +1,310 @@
-package com.example.fitsync
-
-import android.os.Bundle
-import android.os.PersistableBundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import com.example.fitsync.ui.theme.FitSyncTheme
-
-class CalenderActivity: ComponentActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        setContent {
-            FitSyncTheme {
-Text(text = "aaafasf")
-            }
-        }
-    }
-}
+//package com.example.fitsync
+//
+//import android.content.ContentValues.TAG
+//import android.os.Bundle
+//import android.os.PersistableBundle
+//import android.util.Log
+//import androidx.activity.ComponentActivity
+//import androidx.activity.compose.setContent
+//import androidx.compose.foundation.clickable
+//import androidx.compose.foundation.layout.Arrangement
+//import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.Row
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.fillMaxWidth
+//import androidx.compose.foundation.layout.height
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.foundation.layout.width
+//import androidx.compose.material.icons.Icons
+//import androidx.compose.material.icons.filled.ChevronLeft
+//import androidx.compose.material.icons.filled.ChevronRight
+//import androidx.compose.material3.Card
+//import androidx.compose.material3.CardDefaults
+//import androidx.compose.material3.Icon
+//import androidx.compose.material3.IconButton
+//import androidx.compose.material3.MaterialTheme
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.runtime.getValue
+//import androidx.compose.runtime.mutableStateOf
+//import androidx.compose.runtime.remember
+//import androidx.compose.runtime.setValue
+//import androidx.compose.ui.Alignment
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.graphics.Color
+//import androidx.compose.ui.platform.LocalContext
+//import androidx.compose.ui.text.font.FontWeight
+//import androidx.compose.ui.tooling.preview.Preview
+//import androidx.compose.ui.unit.dp
+//import androidx.compose.ui.unit.sp
+//import com.example.fitsync.ui.theme.FitSyncTheme
+//import com.google.firebase.FirebaseApp
+//import com.google.firebase.firestore.ktx.firestore
+//import com.google.firebase.ktx.Firebase
+//import java.time.DayOfWeek
+//import java.time.LocalDate
+//import java.time.YearMonth
+//import java.time.format.DateTimeFormatter
+//import java.util.Locale
+//
+//
+//class CalenderActivity : ComponentActivity() {
+//
+//    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+//        super.onCreate(savedInstanceState, persistentState)
+//        setContent {
+//            FitSyncTheme {
+//                Text(text = "as")
+//            }
+//        }
+//    }
+//}
+//
+//
+//@Composable
+//fun Greeting() {
+//    val db = Firebase.firestore
+//    val context = LocalContext.current
+//    FirebaseApp.initializeApp(context)
+//    var clickedDate by remember {
+//        mutableStateOf(LocalDate.now().year * 10000 + LocalDate.now().monthValue * 100 + LocalDate.now().dayOfMonth)
+//    }
+//    Column {
+//
+//        val dataSource = CalendarDataSource()
+//        var calendarUiModel by remember {
+//            mutableStateOf(dataSource.getData(lastSelectedDate = dataSource.today))
+//        }
+//        var currentYearMonth by remember {
+//            mutableStateOf(YearMonth.now())
+//        }
+//        Header(
+//            data = calendarUiModel,
+//            onPrevClickListener = { startDate ->
+//                val finalStartDate = startDate.minusDays(1)
+//                calendarUiModel = dataSource.getData(
+//                    startDate = finalStartDate,
+//                    lastSelectedDate = calendarUiModel.selectedDate.date
+//                )
+//            },
+//            onNextClickListener = { endDate ->
+//                val finalStartDate = endDate.plusDays(2)
+//                calendarUiModel = dataSource.getData(
+//                    startDate = finalStartDate,
+//                    lastSelectedDate = calendarUiModel.selectedDate.date
+//                )
+//            },
+//            onMinusMonth = { currentYearMonth = currentYearMonth.minusMonths(1) },
+//            onPlusMonth = { currentYearMonth = currentYearMonth.plusMonths(1) }
+//        )
+//
+//        fun convertLocalDateToInt(dateModel: CalendarUiModel.Date): Int {
+//            val date = dateModel.date
+//            return date.year * 10000 + date.monthValue * 100 + date.dayOfMonth
+//        }
+//
+//        Content(
+//            currentYearMonth = currentYearMonth, data = calendarUiModel,
+//            onDateClickListener = { date ->
+//                calendarUiModel = calendarUiModel.copy(
+//                    selectedDate = date,
+//                    visibleDates = calendarUiModel.visibleDates.map {
+//                        it.copy(
+//                            isSelected = it.date.isEqual(date.date)
+//                        )
+//                    }
+//                )
+//
+//                clickedDate = convertLocalDateToInt(calendarUiModel.selectedDate)
+//            },
+//        )
+//
+//        val user = hashMapOf(
+//            "first" to "Ada",
+//            "last" to "Lovelace",
+//            "born" to 1815
+//        )
+//
+//// Add a new document with a generated ID
+////        db.collection("users")
+////            .add(user)
+////            .addOnSuccessListener { documentReference ->
+////                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+////            }
+////            .addOnFailureListener { e ->
+////                Log.w(TAG, "Error adding document", e)
+////            }
+////
+////        db.collection("users")
+////            .get()
+////            .addOnSuccessListener { result ->
+////                for (document in result) {
+////                    Log.d(TAG, "${document.id} => ${document.data}")
+////                }
+////            }
+////            .addOnFailureListener { exception ->
+////                Log.w(TAG, "Error getting documents.", exception)
+////            }
+//    }
+//
+//
+//
+//}
+//
+//@Composable
+//fun Header(
+//    data: CalendarUiModel,
+//    onPrevClickListener: (LocalDate) -> Unit,
+//    onNextClickListener: (LocalDate) -> Unit,
+//    onMinusMonth: () -> Unit,
+//    onPlusMonth: () -> Unit
+//) {
+//    val currentYearMonth = YearMonth.now()
+//    val currentMonth = remember { mutableStateOf(currentYearMonth) }
+//    Column(modifier = Modifier.padding(10.dp)) {
+//        Row {
+//            Text(
+//                text =
+//                currentMonth.value.format(
+//                    DateTimeFormatter.ofPattern(
+//                        "yyyy년 MMMM",
+//                        Locale("ko")
+//                    )
+//                ),
+//                modifier = Modifier.weight(1f),
+//                fontSize = 30.sp
+//            )
+//
+//            IconButton(onClick = {
+//                onPrevClickListener(data.startDate.date)
+//                val newMonth = currentMonth.value.minusMonths(1)
+//                currentMonth.value = newMonth
+//                onMinusMonth()
+//            }) {
+//                Icon(
+//                    imageVector = Icons.Filled.ChevronLeft,
+//                    contentDescription = "Back"
+//                )
+//            }
+//            IconButton(onClick = {
+//                onNextClickListener(data.endDate.date)
+//                val newMonth = currentMonth.value.plusMonths(1)
+//                currentMonth.value = newMonth
+//                onPlusMonth()
+//            }) {
+//                Icon(
+//                    imageVector = Icons.Filled.ChevronRight,
+//                    contentDescription = "Next"
+//                )
+//            }
+//        }
+//    }
+//}
+//
+//@Composable
+//fun Content(
+//    currentYearMonth: YearMonth,
+//    data: CalendarUiModel,
+//    onDateClickListener: (CalendarUiModel.Date) -> Unit,
+////    onDelayedClick: () -> Unit,
+////    onNotDelayedClick: () -> Unit
+//) {
+//    Column {
+//        repeat(6) { rowIndex ->
+//            Row(
+//                modifier = Modifier.fillMaxWidth(),
+//                horizontalArrangement = Arrangement.SpaceBetween
+//            ) {
+//                val startIndex = rowIndex * 7
+//                val endIndex = minOf(startIndex + 7, data.visibleDates.size)
+//                for (i in startIndex until endIndex) {
+//                    val date = data.visibleDates[i]
+//                    ContentItem(
+//                        currentYearMonth = currentYearMonth,
+//                        date = date,
+//                        onDateClickListener,
+////                        onDelayedClick = onDelayedClick,
+////                        onNotDelayedClick = onNotDelayedClick
+//                    )
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//@Composable
+//fun ContentItem(
+//    currentYearMonth: YearMonth,
+//    date: CalendarUiModel.Date,
+//    onClickListener: (CalendarUiModel.Date) -> Unit,
+////    onDelayedClick: () -> Unit,
+////    onNotDelayedClick: () -> Unit
+//) {
+//    val textColor =
+//        if (date.date.year == currentYearMonth.year && date.date.month == currentYearMonth.month) {
+//            if (date.date.dayOfWeek == DayOfWeek.SUNDAY) {
+//                Color.Red
+//            } else if (date.date.dayOfWeek == DayOfWeek.SATURDAY) {
+//                Color.Blue
+//            } else {
+//                MaterialTheme.colorScheme.primary
+//            }
+//        } else {
+//            MaterialTheme.colorScheme.onBackground.copy(alpha = 0.6f)
+//        }
+//    val backgroundColor = if (date.isToday && !date.isSelected) {
+//        Color.LightGray
+//    } else if (date.isSelected) {
+//        Color(0xFFF781F3).copy(alpha = 0.7f)
+//    } else {
+//        Color.White
+//    }
+//    var lastClickTime by remember {
+//        mutableStateOf(0L)
+//    }
+//    var clickCount by remember { mutableStateOf(0) }
+//    Card(
+//        modifier = Modifier
+//            .width(45.dp)
+//            .height(45.dp)
+//            .clickable {
+//                clickCount++
+//                val currentTime = System.currentTimeMillis()
+////                if (currentTime - lastClickTime < 500) {
+////                    onDelayedClick()
+////                } else {
+////                    onNotDelayedClick()
+////                }
+//                lastClickTime = currentTime
+//                onClickListener(date)
+//            },
+//        colors = CardDefaults.cardColors(
+//            backgroundColor
+//        ),
+//    ) {
+//        Column(
+//            modifier = Modifier
+//                .fillMaxSize(),
+//            horizontalAlignment = Alignment.CenterHorizontally
+//        ) {
+//            Text(
+//                text = date.day,
+//                modifier = Modifier.align(Alignment.CenterHorizontally),
+//                style = MaterialTheme.typography.bodySmall.copy(fontWeight = FontWeight.SemiBold),
+//                color = textColor
+//            )
+//            Text(
+//                text = date.date.dayOfMonth.toString(),
+//                modifier = Modifier.align(Alignment.CenterHorizontally),
+//                style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.SemiBold),
+//                color = textColor
+//            )
+//        }
+//    }
+//}
+//
+//@Preview
+//@Composable
+//fun AODNSD() {
+//    Greeting()
+//}
