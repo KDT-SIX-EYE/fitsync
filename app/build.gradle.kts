62a7918d993c4ce76357d3ffb05db74f73df1ce6
상은 <71699054+vmkmym@users.noreply.github.com>
2023-08-27 12:17:10 +0900

    Messenger Activity 50% 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,23 +60,29 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("com.google.firebase:firebase-firestore-ktx:24.7.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
-    implementation("com.google.firebase:firebase-common-ktx:20.3.3")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.leanback:leanback:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    // 파이어스토어
+
     implementation(platform("com.google.firebase:firebase-bom:32.2.2"))
     implementation("com.google.firebase:firebase-analytics-ktx")
-    // 로그인 그래들
     implementation("com.google.firebase:firebase-auth-ktx")
-    // 뷰모델
+    implementation("com.google.firebase:firebase-firestore-ktx:24.7.1")
+    implementation("com.google.firebase:firebase-common-ktx:20.3.3")
+    implementation("com.google.firebase:firebase-database-ktx:20.2.2")
+
     val lifecycleVersion = "2.6.1"
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+
+    implementation("androidx.compose.material:material:1.5.0")
 }
--- app/src/main/java/com/example/fitsync/MessengerActivity.kt
+++ app/src/main/java/com/example/fitsync/MessengerActivity.kt
@@ -1,154 +1,291 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package com.example.fitsync
 
-import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import com.example.fitsync.Message.Message
-import com.example.fitsync.ui.theme.FitSyncTheme
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.runtime.*
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.res.painterResource
-import androidx.lifecycle.ViewModelProvider
-
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.fitsync.Message.ChatMessage
+import com.example.fitsync.ui.theme.FitSyncTheme
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase.getInstance
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MessengerActivity : ComponentActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            val db = Firebase.firestore
-            val messages = remember { mutableStateListOf<Message>() }
-            // ViewModelProvider 클래스의 인스턴스를 가져오기
-            val viewModel = ViewModelProvider(this).get(MessengerViewModel::class.java)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        setContent {
             FitSyncTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = Color.White
+                    color = MaterialTheme.colorScheme.background
                 ) {
-                    MessengerScreen()
+                    ChatScreen(firebaseAuth = firebaseAuth)
                 }
             }
-
-            // Firestore에서 메시지 가져오기
-            db.collection("messages")
-                .orderBy("timestamp")
-                .addSnapshotListener { querySnapshot, error ->
-                    if (error != null) return@addSnapshotListener
-
-                    val messageList = mutableListOf<Message>()
-                    querySnapshot?.documents?.forEach { document ->
-                        val message = document.toObject(Message::class.java)
-                        message?.let { messageList.add(it) }
-                    }
-                    messages.clear()
-                    messages.addAll(messageList)
-                }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MessengerScreen() {
+private fun ChatScreen(firebaseAuth: FirebaseAuth) {
+    val user: FirebaseUser? = firebaseAuth.currentUser
+    var composingMessage by remember { mutableStateOf("") }
+    var sentMessages by remember { mutableStateOf(listOf<ChatMessage>()) }
+    val context = LocalContext.current
+
+    // 이전 채팅 메시지 불러오기
+    loadChatMessages { messages ->
+        sentMessages = messages
+    }
+
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text("대화 상대 이름") },
+                title = { Text(text = "Messenger", fontSize = 17.sp, fontFamily = FontFamily.SansSerif) },
                 navigationIcon = {
-                    IconButton(onClick = { /* 뒤로 가기 동작 */ }) {
-                        Icon(imageVector = Icons.Filled.ArrowBack,
-                            contentDescription = "뒤로 가기")
+                    IconButton(onClick = { /* 채팅 목록 액티비티로 이동 */
+//                        val intent = Intent(context, ChatListActivity::class.java)
+//                        context.startActivity(intent)
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "뒤로 가기"
+                        )
                     }
                 },
                 actions = {
-                    IconButton(onClick = { /* 검색 동작 */ }) {
-                        Icon(imageVector = Icons.Filled.Search,
-                            contentDescription = "검색")
+                    IconButton(onClick = { /* 채팅 내용 검색 동작 */ }) {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "검색"
+                        )
                     }
                 }
             )
         },
         bottomBar = {
-            Row(
+            BottomAppBar(
+                containerColor = Color.White
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    IconButton(onClick = { /* 더보기 버튼을 누르면 뭐가 나와야 할까 */ }) {
+                        Icon(
+                            imageVector = Icons.Default.Add,
+                            contentDescription = "더보기")
+                    }
+                    CustomTextField(
+                        value = composingMessage,
+                        onValueChange = { composingMessage = it },
+                        onSendClick = {
+                            if (composingMessage.isNotEmpty()) {
+                                val currentDate = SimpleDateFormat("HH:mm", Locale.getDefault()).format(Date())
+                                val newChatMessage =
+                                    ChatMessage(
+                                        // data class에 맞춰 바꾸기 (로그인)
+                                        message = composingMessage,
+                                        userId = user?.uid,
+                                        userName = user?.displayName,
+                                        uploadDate = currentDate)
+//                                sentMessages += composingMessage
+                                saveChatMessage(newChatMessage)
+                                composingMessage = ""
+                            }
+                        }
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        LazyColumn(
+            reverseLayout = true,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            items(sentMessages.reversed()) { message ->
+                ChatItemBubble(
+                    message = message,
+                    UserId = user?.uid
+                )
+//                Box(
+//                    modifier = Modifier
+//                        .fillMaxWidth()
+//                        .padding(end = 20.dp, top = 10.dp, bottom = 10.dp),
+//                    contentAlignment = Alignment.BottomEnd
+//                ) {
+//
+//                }
+            }
+        }
+    }
+}
+
+fun loadChatMessages(listener: (List<ChatMessage>) -> Unit) {
+    val database = getInstance("https://fit-sync-76834-default-rtdb.asia-southeast1.firebasedatabase.app/")
+    val chatRef = database.getReference("LoadChat")
+
+    chatRef.addValueEventListener(object : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) {
+            val messages = mutableListOf<ChatMessage>()
+            for (childSnapshot in snapshot.children) {
+                val message = childSnapshot.getValue(ChatMessage::class.java)
+                message?.let {
+                    messages.add(it)
+                }
+            }
+            listener(messages)
+        }
+        override fun onCancelled(error: DatabaseError) {
+            // 에러 처리
+        }
+    })
+}
+
+
+
+fun saveChatMessage(chatmessage: ChatMessage) {
+    val database = getInstance("https://dataclass-27aac-default-rtdb.asia-southeast1.firebasedatabase.app/")
+    val chatRef = database.getReference("SaveChat")
+    val newMessageRef = chatRef.push() // 새로운 메시지를 추가하기 위한 참조
+
+    newMessageRef.setValue(chatmessage)
+}
+
+
+@Composable
+fun CustomTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    onSendClick: () -> Unit,
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        BasicTextField(
+            value = value,
+            onValueChange = onValueChange,
+            modifier = Modifier
+                .weight(1f) // 여기서 비율을 조정
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+                .background(Color.White),
+            textStyle = LocalTextStyle.current.copy(fontSize = 16.sp)
+        )
+
+        IconButton(
+            onClick = onSendClick
+        ) {
+            Icon(
+                imageVector = Icons.Default.Send,
+                contentDescription = "메시지 전송 버튼"
+            )
+        }
+    }
+}
+
+@Composable
+fun ChatItemBubble(
+    message: ChatMessage,
+    UserId: String?
+) {
+    val isCurrentUserMessage = UserId == message.userId
+    val bubbleColor = if (isCurrentUserMessage) Color.DarkGray else Color.Blue // 사용자와 상대방 메시지에 다른 색 지정
+    val alignment = if (isCurrentUserMessage) Alignment.BottomEnd else Alignment.BottomStart
+    Column {
+        if (!isCurrentUserMessage) {
+            Text(text = message.userName ?: "")
+        }
+        Row {
+            if (isCurrentUserMessage){
+                Text(text = message.uploadDate ?: "")
+            }
+            Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(8.dp),
-                verticalAlignment = Alignment.CenterVertically
+                    .padding(7.dp)
+                    .background(bubbleColor),
+                contentAlignment = alignment
             ) {
-                Icon(
-                    imageVector = Icons.Filled.Add,
-                    contentDescription = "이미지 불러오기",
-                    modifier = Modifier.size(24.dp)
-                )
-                Spacer(modifier = Modifier.width(8.dp))
-                RoundedTextField(modifier = Modifier.weight(1f))
+                Text(
+                    text = message.message ?: "",
+                    color = Color.White,
+                    modifier = Modifier.padding(8.dp))
             }
-        },
-        content = {
-            ConversationContent(it)
+            if (!isCurrentUserMessage) {
+                Text(
+                    text = message.uploadDate ?: "",
+                    color = Color.Black,
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
         }
-    )
+    }
 }
 
-@Composable
-fun RoundedTextField(modifier: Modifier) {
-    BasicTextField(
-        value = "",
-        onValueChange = {},
-        modifier = modifier
-            .background(MaterialTheme.colorScheme.primary)
-            .padding(16.dp)
-            .fillMaxHeight()
-            .background(MaterialTheme.colorScheme.surface)
-            .border(1.dp, MaterialTheme.colorScheme.primary, shape = MaterialTheme.shapes.large)
-    )
-}
 
+@Preview(showBackground = true)
 @Composable
-fun ConversationContent(paddingValues: PaddingValues) {
-    // 여기서 대화 내용을 보여줄 부분을 구현하세요.
-    // 대화 말풍선을 조건에 따라 생성하고 배치하는 코드를 작성합니다.
+fun GreetingPreview() {
+    val firebaseAuth = FirebaseAuth.getInstance() // Firebase 초기화 및 인스턴스 생성
+    FitSyncTheme {
+        ChatScreen(firebaseAuth = firebaseAuth)
+    }
 }
