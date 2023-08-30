ef4e7bbad9a82b0fd1979156b149fbac549fdea8
JD131313 <dbalfh@gmail.com>
2023-08-30 15:26:50 +0900

    3까지 최종

--- app/src/main/java/com/example/fitsync/Membership.kt
+++ app/src/main/java/com/example/fitsync/Membership.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -11,8 +12,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -26,7 +30,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
@@ -57,10 +63,19 @@ class Membership : ComponentActivity() {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
+                .padding(top = 76.dp, start = 16.dp, end = 16.dp, bottom = 0.dp),
+            verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.fitsync),
+                contentDescription = "Profile Image",
+                modifier = Modifier
+                    .size(200.dp)
+                    .clip(CircleShape)
+                    .align(Alignment.CenterHorizontally)
+            )
             var selectedRole by remember { mutableStateOf("") }
             DropDownMenued(selectedRole) { role ->
                 selectedRole = role
@@ -69,7 +84,10 @@ class Membership : ComponentActivity() {
             OutlinedTextField(
                 value = name,
                 onValueChange = { name = it },
-                label = { Text("이름") },
+                label = { Text("이름을 입력하세요") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
                 keyboardOptions = KeyboardOptions.Default.copy(
                     imeAction = ImeAction.Next
                 ),
@@ -78,7 +96,10 @@ class Membership : ComponentActivity() {
             OutlinedTextField(
                 value = email,
                 onValueChange = { email = it },
-                label = { Text("이메일") },
+                label = { Text("이메일을 입력하세요") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
                 keyboardOptions = KeyboardOptions.Default.copy(
                     imeAction = ImeAction.Next
                 ),
@@ -87,7 +108,10 @@ class Membership : ComponentActivity() {
             OutlinedTextField(
                 value = password,
                 onValueChange = { password = it },
-                label = { Text("비밀번호") },
+                label = { Text("비밀번호를 입력하세요") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
                 visualTransformation = PasswordVisualTransformation(),
                 keyboardOptions = KeyboardOptions.Default.copy(
                     imeAction = ImeAction.Done
@@ -128,7 +152,13 @@ class Membership : ComponentActivity() {
                         signupErrorMessage = "이름, 이메일, 비밀번호, 근로 종류를 모두 입력해주세요."
                     }
                 },
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                colors = ButtonDefaults.buttonColors(
+                    Color.Black,
+                    contentColor = Color.White
+                )
             ) {
                 Text("회원가입")
             }
@@ -143,6 +173,7 @@ class Membership : ComponentActivity() {
         }
     }
 
+
     private fun navigateToMainActivity() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
@@ -156,7 +187,7 @@ fun DropDownMenu(selectedItem: String, onChangeItem: (String) -> Unit) {
     var expanded by remember { mutableStateOf(false) }
     val list = listOf("trainer", "manager", "Arbeit")
 
-    Column(modifier = Modifier.padding(20.dp)) {
+    Column(modifier = Modifier.padding(20.dp).fillMaxWidth()) {
         ExposedDropdownMenuBox(
             expanded = expanded,
             onExpandedChange = { expanded = !expanded }
--- app/src/main/java/com/example/fitsync/ScheduleManagement.kt
+++ app/src/main/java/com/example/fitsync/ScheduleManagement.kt
@@ -1,7 +1,6 @@
 package com.example.fitsync
 
 import android.content.ContentValues.TAG
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -52,195 +51,24 @@ import java.time.format.DateTimeFormatter
 import java.util.Locale
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material.icons.filled.Face
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.*
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.TextFieldValue
 
 class ScheduleManagement : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FinalScheduleManagementScreen()
+            val db = Firebase.firestore
+            ScheduleManagementScreen(db)
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FinalScheduleManagementScreen() {
-    val context = LocalContext.current
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(
-                        text = "Fit Sync",
-                        fontSize = 24.sp,
-                        fontFamily = FontFamily.SansSerif,
-                        fontWeight = FontWeight.ExtraBold
-                    )
-                },
-                navigationIcon = {
-                    IconButton(onClick = { /* 메뉴 아이콘 */ }) {
-                        Icon(
-                            imageVector = Icons.Default.Menu,
-                            contentDescription = "메뉴 아이콘"
-                        )
-                    }
-                },
-                actions = {
-                    IconButton(onClick = {
-                        val intent = Intent(context, MyProfileActivity::class.java)
-                        context.startActivity(intent)
-                    }) {
-                        Icon(
-                            imageVector = Icons.Default.Face,
-                            contentDescription = "사용자 프로필"
-                        )
-                    }
-                }
-            )
-        },
-        bottomBar = {
-            BottomAppBar(
-                containerColor = Color.White
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    val context = LocalContext.current
-
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        IconButton(onClick = {
-                            val intent = Intent(context, CalendarActivity::class.java)
-                            context.startActivity(intent)
-                        }) {
-                            Icon(
-                                imageVector = Icons.Default.DateRange,
-                                contentDescription = "캘린더 액티비티로 이동"
-                            )
-                        }
-                        Text(
-                            text = "캘린더",
-                            fontSize = 10.sp,
-                            fontFamily = FontFamily.SansSerif,
-                            modifier = Modifier.padding(top = 0.dp)
-                        )
-                    }
-
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        IconButton(onClick = {
-                            val intent = Intent(context, MainActivity::class.java)
-                            context.startActivity(intent)
-                        }) {
-                            Icon(
-                                painter = painterResource(id = R.drawable.baseline_home_24),
-                                contentDescription = "메인 액티비티(홈)으로 이동"
-                            )
-                        }
-                        Text(
-                            text = "Home",
-                            fontSize = 10.sp,
-                            fontFamily = FontFamily.SansSerif,
-                            modifier = Modifier.padding(top = 0.dp)
-                        )
-                    }
-
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        IconButton(onClick = {
-                            val intent = Intent(context, AttendanceActivity::class.java)
-                            context.startActivity(intent)
-                        }) {
-                            Icon(
-                                painter = painterResource(id = R.drawable.baseline_qr_code_2_24),
-                                contentDescription = "QR 액티비티로 이동"
-                            )
-                        }
-                        Text(
-                            text = "QR",
-                            fontSize = 10.sp,
-                            fontFamily = FontFamily.SansSerif,
-                            modifier = Modifier.padding(top = 0.dp)
-                        )
-                    }
-
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        IconButton(onClick = {
-                            val intent = Intent(context, UsersActivity::class.java)
-                            context.startActivity(intent)
-                        }) {
-                            Icon(
-                                imageVector = Icons.Default.AccountCircle,
-                                contentDescription = "사용자 목록 액티비티로 이동"
-                            )
-                        }
-                        Text(
-                            text = "프로필",
-                            fontSize = 10.sp,
-                            fontFamily = FontFamily.SansSerif,
-                            modifier = Modifier.padding(top = 0.dp)
-                        )
-                    }
-
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        IconButton(onClick = {
-                            val intent = Intent(context, MessengerActivity::class.java)
-                            context.startActivity(intent)
-                        }) {
-                            Icon(
-                                painter = painterResource(id = R.drawable.baseline_mark_chat_unread_24),
-                                contentDescription = "메신저 액티비티로 이동"
-                            )
-                        }
-                        Text(
-                            text = "채팅방",
-                            fontSize = 10.sp,
-                            fontFamily = FontFamily.SansSerif,
-                            modifier = Modifier.padding(top = 0.dp)
-                        )
-                    }
-                }
-            }
-        }) { innerPadding ->
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-        ) {
-            item {
-                ScheduleManagementScreen()
-            }
-        }
-    }
-
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun ScheduleManagementScreen() {
-    val db = Firebase.firestore
+fun ScheduleManagementScreen(db: FirebaseFirestore) {
     var calendarOpen by remember {
         mutableStateOf(false)
     }
